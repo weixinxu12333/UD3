9c8813a9dd3616ed8c1e5c58ac0beab13e8137b2
Weixin <weixin.xu.2018@gmail.com>
2020-12-23 13:49:35 +0100

    Ejercicio 2 de UF3

---
+++ UF3-EJ2/UF3-EJ2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UF3_EJ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 5;
+            double A = 4.56;
+            char C = 'a';
+            int c = (int)C;
+            Console.WriteLine("N={0}, A={1}, C={2}, Suma de N+A={3}, Diferencia de A-N={4}, num char de C={5}",
+                N, A, C, N + A, A - N, c);
+        }
+    }
+}
