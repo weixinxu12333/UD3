fe16fcff95d11dc24d13e9bee038595e75c9dafd
Weixin <weixin.xu.2018@gmail.com>
2020-12-23 13:47:47 +0100

    Ejercicio 1 de UF3

---
+++ UF3-EJ1/UF3-EJ1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UF3_EJ1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 10;
+            int n2 = 5;
+
+            int suma = n1 + n2;
+            int resta = n1 - n2;
+            int multiplica = n1 * n2;
+            int dividir = n1 / n2;
+            int mod = n1 % n2;
+
+            Console.WriteLine("suma: {0}, resta: {1}, multiplica: {2}, dividir: {3}, mod: {4}",
+                suma, resta, multiplica, dividir, mod);
+
+            Console.ReadKey();
+        }
+    }
+}
