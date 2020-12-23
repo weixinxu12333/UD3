16febb543f0dd289de8130d2b821c897fb82665d
Weixin <weixin.xu.2018@gmail.com>
2020-12-23 13:56:52 +0100

    Ejercicio 5 de UF3

---
+++ UF3-EJ5/UF3-EJ5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UF3_EJ5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 1;
+            int B = 2;
+            int C = 3;
+            int D = 4;
+
+            B = C;
+            Console.WriteLine(B);
+            C = A;
+            Console.WriteLine(C);
+            A = D;
+            Console.WriteLine(A);
+            D = B;
+            Console.WriteLine(D);
+        }
+    }
+}
