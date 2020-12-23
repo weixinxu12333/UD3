ccd27c42b400386e88b3cbbbd6d049f4a28699dc
Weixin <weixin.xu.2018@gmail.com>
2020-12-23 13:56:09 +0100

    Ejercicio 4 de UF3

---
+++ UF3-EJ4/UF3-EJ4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UF3_EJ4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 1;
+            Console.WriteLine(N);
+            N = N + 77;
+            Console.WriteLine(N);
+            N = N - 3;
+            Console.WriteLine(N);
+            N = N * 2;
+            Console.WriteLine(N);
+        }
+    }
+}
